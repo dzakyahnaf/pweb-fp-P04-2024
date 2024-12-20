85288160310dd53b1fba55c0bacfaac4d731a7d1
NoOne72 <144348985+NoOne72@users.noreply.github.com>
2024-12-20 17:38:48 +0700

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,7 +1,8 @@
 <template>
-  <div class="w-full h-screen flex justify-center items-center bg-peri">
-    <div class="p-4 text-cloud text-2xl font-playfair rounded">
-      Hello world!
-    </div>
+  <div>
+    <!-- Nuxt Page -->
+    <NuxtPage />
   </div>
 </template>
+
+
