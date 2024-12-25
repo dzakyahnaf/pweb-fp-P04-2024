1572cf0c218058e2742e57f40d0ca071184ffd25
NoOne72 <144348985+NoOne72@users.noreply.github.com>
2024-12-25 19:23:05 +0700

    Update rules.vue

--- pages/rules.vue
+++ pages/rules.vue
@@ -3,12 +3,28 @@
     class="min-h-screen bg-cover bg-center bg-no-repeat flex flex-col"
     :style="{ backgroundImage: `url(${backgroundImage})` }"
   >
-    <!-- Judul -->
-    <div class="text-center mt-8">
-      <h1 class="text-4xl font-bold text-white bg-black bg-opacity-50 rounded-lg inline-block px-6 py-2">
-        Peraturan Kost
-      </h1>
-    </div>
+     <!-- Navbar -->
+     <nav
+      class="bg-gray-800 shadow-md py-4 px-4 sm:px-8 flex justify-between items-center sticky top-0 z-10"
+    >
+      <div class="text-2xl font-bold text-white">sinKOSTan</div>
+      <div class="space-x-4 flex flex-wrap items-center">
+        <NuxtLink
+          to="/"
+          class="text-white hover:text-blue-600"
+          :class="currentRoute === '/' ? 'text-blue-400' : ''"
+        >
+          Login
+        </NuxtLink>
+        <NuxtLink
+          to="/facility"
+          class="text-white hover:text-blue-600"
+          :class="currentRoute === '/facility' ? 'text-blue-400' : ''"
+        >
+          Fasilitas
+        </NuxtLink>
+      </div>
+    </nav>
 
     <!-- Container untuk Peraturan -->
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 p-8">
@@ -103,6 +119,11 @@
       </div>
     </div>
   </div>
+  <footer class="bg-gray-800 text-white py-6">
+      <div class="container mx-auto px-4 text-center">
+        <p class="text-sm">&copy; 2024 sinKOSTan. P04. All rights reserved.</p>
+      </div>
+    </footer>
 </template>
 
 <script>
